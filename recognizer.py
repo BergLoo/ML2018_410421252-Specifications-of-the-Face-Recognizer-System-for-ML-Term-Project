34a765a7e15fe429ee4acb8958af947f53a47485
Berg Loo <myland1997@hotmail.com>
2018-07-08 20:47:56 +0800

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -2,10 +2,11 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import os
+
 add = os.getcwd()
 
-
 root = Tk()
+
 #setting up a tkinter canvas with scrollbars
 frame = Frame(root, bd=2, relief=SUNKEN)
 frame.grid_rowconfigure(0, weight=1)
@@ -20,19 +21,7 @@ xscroll.config(command=canvas.xview)
 yscroll.config(command=canvas.yview)
 frame.pack(fill=BOTH,expand=1)
 
-
-#function to be called when mouse is clicked
-def printcoords():
-    File = filedialog.askopenfilename(parent=root, initialdir=add,title='Choose an image.')
-    filename = ImageTk.PhotoImage(Image.open(File))
-    canvas.image = filename  # <--- keep reference of your image
-    canvas.create_image(0,0,anchor='nw',image=filename)
-
-Button(root,text='choose',command=printcoords).pack()
-
-
 imlist = [ [None] * 16 for i in range(51) ]
-
 for i in range(1,51):
     for j in (1,2,3,4,6,7,8,10,11,12,13,14,15):
         if (i < 10):
@@ -45,9 +34,31 @@ for i in range(1,51):
                 imlist[i][j] = Image.open('Face Database/'+'s'+str(i)+'_0'+str(j)+'.jpg')
             else:
                 imlist[i][j] = Image.open('Face Database/'+'s'+str(i)+'_'+str(j)+'.jpg')
+#imlist[50][15].show()
+
+#function to be called when mouse is clicked
+
+def printcoords():
+    File = filedialog.askopenfilename(parent=root, initialdir=add,title='Choose an image.')
+    filename = ImageTk.PhotoImage(Image.open(File))
+    canvas.image = filename  # <--- keep reference of your image
+    canvas.create_image(0,0,anchor='nw',image=filename)
+
+def train():
+    pass
+    
+    
+    
+def test():
+    pass
+    
+    
+    
+    
+Button(root,text='choose',command=printcoords).pack()
+Button(root,text='train',command=train).pack()
+Button(root,text='test',command=test).pack()
+
 print('XD')
-
-imlist[50][15].show()
-
-
 root.mainloop()
+print('XD')
