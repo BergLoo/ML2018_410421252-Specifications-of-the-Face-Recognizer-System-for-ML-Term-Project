1ff3f95f3306fa1051ad7a42e99687e0a0719b7c
Berg Loo <myland1997@hotmail.com>
2018-07-08 20:38:10 +0800

    Update recognizer.py

--- recognizer.py
+++ recognizer.py
@@ -2,35 +2,35 @@ from tkinter import *
 from tkinter import filedialog
 from PIL import Image, ImageTk
 import os
+
 add = os.getcwd()
 
-if __name__ == "__main__":
-    root = Tk()
+root = Tk()
 
-    #setting up a tkinter canvas with scrollbars
-    frame = Frame(root, bd=2, relief=SUNKEN)
-    frame.grid_rowconfigure(0, weight=1)
-    frame.grid_columnconfigure(0, weight=1)
-    xscroll = Scrollbar(frame, orient=HORIZONTAL)
-    xscroll.grid(row=1, column=0, sticky=E+W)
-    yscroll = Scrollbar(frame)
-    yscroll.grid(row=0, column=1, sticky=N+S)
-    canvas = Canvas(frame, bd=0, xscrollcommand=xscroll.set, yscrollcommand=yscroll.set)
-    canvas.grid(row=0, column=0, sticky=N+S+E+W)
-    xscroll.config(command=canvas.xview)
-    yscroll.config(command=canvas.yview)
-    frame.pack(fill=BOTH,expand=1)
+#setting up a tkinter canvas with scrollbars
+frame = Frame(root, bd=2, relief=SUNKEN)
+frame.grid_rowconfigure(0, weight=1)
+frame.grid_columnconfigure(0, weight=1)
+xscroll = Scrollbar(frame, orient=HORIZONTAL)
+xscroll.grid(row=1, column=0, sticky=E+W)
+yscroll = Scrollbar(frame)
+yscroll.grid(row=0, column=1, sticky=N+S)
+canvas = Canvas(frame, bd=0, xscrollcommand=xscroll.set, yscrollcommand=yscroll.set)
+canvas.grid(row=0, column=0, sticky=N+S+E+W)
+xscroll.config(command=canvas.xview)
+yscroll.config(command=canvas.yview)
+frame.pack(fill=BOTH,expand=1)
 
 
-    #function to be called when mouse is clicked
-    def printcoords():
-        File = filedialog.askopenfilename(parent=root, initialdir=add,title='Choose an image.')
-        filename = ImageTk.PhotoImage(Image.open(File))
-        canvas.image = filename  # <--- keep reference of your image
-        canvas.create_image(0,0,anchor='nw',image=filename)
+#function to be called when mouse is clicked
+def printcoords():
+    File = filedialog.askopenfilename(parent=root, initialdir=add,title='Choose an image.')
+    filename = ImageTk.PhotoImage(Image.open(File))
+    canvas.image = filename  # <--- keep reference of your image
+    canvas.create_image(0,0,anchor='nw',image=filename)
 
-    Button(root,text='choose',command=printcoords).pack()
-    root.mainloop()
+Button(root,text='choose',command=printcoords).pack()
+
 
 imlist = [ [None] * 16 for i in range(51) ]
 
@@ -40,3 +40,8 @@ for i in range(1,51):
             if(j < 5):
                 imlist[i][j] = Image.open('Face Database/'+'s0'+str(i)+'_0'+str(j)+'.jpg')
 print('XD')
+
+
+
+
+root.mainloop()
